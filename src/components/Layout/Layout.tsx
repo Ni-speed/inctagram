88193512141dd5c95b20f280e1356120d0eac000
Ni-speed <railmir@gmail.com>
2023-10-12 11:56:26 +0500

    I-0000 feat: add basic page layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import {NextPage,} from "next";
+import {PropsWithChildren, ReactElement} from "react";
+import styles from "@/styles/Home.module.css";
+import {Navbar} from "@/components/Navbar/Navbar";
+
+export const Layout: NextPage<PropsWithChildren<{}>> =({children}) => {
+    return (
+        <main className={`${styles.main}`}>
+            <Navbar/>
+            {children}
+        </main>
+    );
+}
+export const getLayout = (page: ReactElement)=>{
+    return <Layout>{page}</Layout>
+}
