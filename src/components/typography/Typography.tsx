476539321598ed2f3fe736d535489073b0ed2c21
Ni-speed <railmir@gmail.com>
2023-10-14 20:17:57 +0500

    I-0000 feat: add typography component

---
+++ src/components/typography/Typography.tsx
@@ -0,0 +1,27 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+
+import s from './typography.module.scss'
+
+type TypographyProps<T extends ElementType = 'p'> = {
+  as?: T
+  className?: string
+  variant?:
+    | 'boldText14'
+    | 'boldText16'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'large'
+    | 'mediumText14'
+    | 'regularLink'
+    | 'regularText14'
+    | 'regularText16'
+    | 'semiBoldSmallText'
+    | 'smallLink'
+    | 'smallText'
+} & ComponentPropsWithoutRef<T>
+export const Typography = <T extends ElementType = 'p'>(props: TypographyProps<T>) => {
+  const { as: Component = 'p', className, variant = 'body1', ...rest } = props
+
+  return <Component className={`${s[variant]} ${className}`} {...rest}></Component>
+}
