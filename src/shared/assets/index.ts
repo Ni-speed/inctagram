9122e6444268b9b1326886adcfa3c1988ab5e53b
Yelkira <penda.fu@gmail.com>
2023-10-18 23:11:52 +0300

    I-0000 feat: some imports

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,2 +1,5 @@
+export { ArrowDownIcon } from './svg/arrowDownIcon'
+export { ArrowLeftIcon } from './svg/arrowLeftIcon'
+export { ArrowRightIcon } from './svg/arrowRightIcon'
 export { СheckboxChecked } from './svg/checkboxChecked/checkbox-checked'
 export { CheckboxCheckedDisabled } from './svg/checkboxCheckedDisabled/checkbox-checked-disabled'
