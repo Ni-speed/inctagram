48c126f841ec18b163978e0c31c46421e683690a
IvanBichan <ivanbichanfe@gmail.com>
2023-10-19 13:26:47 +0200

    sidebars feat: creat a universal sidebar item component

---
+++ src/shared/ui/sidebars/sidebarItems/SidebarItem.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react'
+
+import { Typography } from '@/shared/ui'
+import { clsx } from 'clsx'
+
+import s from './SidebarItem.module.scss'
+
+export type CreateSidebarItemProps = {
+  callback?: () => void
+  children: ReactNode
+  className?: string
+  title: string
+}
+export const SidebarItem = ({ callback, children, className, title }: CreateSidebarItemProps) => {
+  const classNames = {
+    container: clsx(s.container, className),
+  }
+  const onClickHandler = () => {
+    callback && callback()
+  }
+
+  return (
+    <div className={classNames.container} onClick={onClickHandler}>
+      {children}
+      <Typography variant={'mediumText14'}>{title}</Typography>
+    </div>
+  )
+}
