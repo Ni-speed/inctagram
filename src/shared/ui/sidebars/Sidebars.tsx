def3e2275a89522252acbde296f3261bee71cebd
IvanBichan <ivanbichanfe@gmail.com>
2023-10-19 15:09:47 +0200

    sidebars fix: fix code for use universal components

--- src/shared/ui/sidebars/Sidebars.tsx
+++ src/shared/ui/sidebars/Sidebars.tsx
@@ -1,13 +1,15 @@
 import {
-  CreateSidebarItem,
-  FavoritesSidebarItem,
-  LogOutItem,
-  MessengerSidebarItem,
-  MyProfileSidebarItem,
-  SearchSidebarItem,
-  StatisticsSidebarItem,
-} from './sidebarItems'
+  CreateIcon,
+  FavoritesIcon,
+  LogOutIcon,
+  MessengerIcon,
+  MyProfileIcon,
+  SearchIcon,
+  StatisticsIcon,
+} from '@/shared/assets/svg/sidebarsIcons'
+import { SidebarsUniversal } from '@/shared/ui/sidebars/SidebarsUniversal '
 import { HomeSidebarItem } from '@/shared/ui/sidebars/sidebarItems/HomeSidebarItem'
+import { SidebarItem } from '@/shared/ui/sidebars/sidebarItems/SidebarItem'
 import Link from 'next/link'
 
 import s from './Sidebar.module.scss'
@@ -15,22 +17,36 @@ import s from './Sidebar.module.scss'
 export const Sidebars = () => {
   return (
     <div className={s.sidebarsContainer}>
-      <div className={s.sidebarsControl}>
+      <SidebarsUniversal className={s.sidebarsControl}>
         <Link className={s.sidebarItem} href={'/'}>
           <HomeSidebarItem />
         </Link>
-        <CreateSidebarItem />
-        <MyProfileSidebarItem />
-        <MessengerSidebarItem />
-        <SearchSidebarItem />
-      </div>
-      <div className={s.sidebarsStatistic}>
-        <StatisticsSidebarItem />
-        <FavoritesSidebarItem />
-      </div>
-      <div className={s.sidebarsLogout}>
-        <LogOutItem />
-      </div>
+        <SidebarItem title={'Create'}>
+          <CreateIcon />
+        </SidebarItem>
+        <SidebarItem title={'My Profile'}>
+          <MyProfileIcon />
+        </SidebarItem>
+        <SidebarItem title={'Messenger'}>
+          <MessengerIcon />
+        </SidebarItem>
+        <SidebarItem title={'Search'}>
+          <SearchIcon />
+        </SidebarItem>
+      </SidebarsUniversal>
+      <SidebarsUniversal className={s.sidebarsStatistic}>
+        <SidebarItem title={'Statistics'}>
+          <StatisticsIcon />
+        </SidebarItem>
+        <SidebarItem title={'Favorites'}>
+          <FavoritesIcon />
+        </SidebarItem>
+      </SidebarsUniversal>
+      <SidebarsUniversal className={s.sidebarsLogout}>
+        <SidebarItem title={'Log Out'}>
+          <LogOutIcon />
+        </SidebarItem>
+      </SidebarsUniversal>
     </div>
   )
 }
