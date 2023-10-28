284c70fc6b6cffe5467cb6aab4d128a1e18de1dc
Ni-speed <railmir@gmail.com>
2023-10-28 19:52:49 +0500

    I-0000 feat: add useSession

--- src/shared/ui/navbar/Navbar.tsx
+++ src/shared/ui/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { useTranslation } from '../../hooks/useTranslation'
 import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 import s from './Navbar.module.scss'
 
@@ -12,11 +13,20 @@ export const Navbar = () => {
     { id: 2, path: '/signUp', title: t.navBar.signUp },
     { id: 3, path: '/signIn', title: t.navBar.signIn },
     { id: 4, path: '/contacts', title: t.navBar.contacts },
-    { id: 5, path: '/profile', title: t.navBar.profile },
+    // { id: 5, path: '/profile', title: t.navBar.profile },
     { id: 6, path: '/forgotPassword', title: t.navBar.forgotPassword },
     { id: 7, path: '/privacyPolicy', title: t.navBar.privacyPolicy },
     { id: 8, path: '/termOfService', title: t.navBar.termsOfService },
   ]
+  const session = useSession()
+
+  console.log(session)
+
+  const signOutHandler = () => {
+    signOut({
+      callbackUrl: '/',
+    })
+  }
 
   return (
     <div className={s.navbar}>
@@ -29,6 +39,14 @@ export const Navbar = () => {
               </Link>
             )
           })}
+          {session?.data && <Link href={'/profile'}>{t.navBar.profile}</Link>}
+          {session?.data ? (
+            <Link href={'#'} onClick={signOutHandler}>
+              Sign Out
+            </Link>
+          ) : (
+            <Link href={'/api/auth/signin'}>Sign In</Link>
+          )}
         </div>
       </div>
     </div>
