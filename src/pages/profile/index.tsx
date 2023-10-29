597cd7fa45cd1f54b4df0d648d88dfc36e047aec
Ni-speed <railmir@gmail.com>
2023-10-30 01:17:11 +0500

    I-0000 feat: add middleware for private routes

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -5,14 +5,12 @@ import { useSession } from 'next-auth/react'
 function ProfileIndex() {
   const session = useSession()
 
-  console.log(session)
-
   return (
     <div style={{ marginTop: '100px' }}>
       <h2>Profile of: {session?.data?.user?.name} </h2>
       <h2>Email {session?.data?.user?.email} </h2>
       {session?.data?.user?.image && (
-        <Image alt={'img'} height={200} src={session.data.user.image} width={200} />
+        <Image alt={'img'} height={200} priority src={session.data.user.image} width={200} />
       )}
     </div>
   )
