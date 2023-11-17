f0e9613ca14b185a2a5079efac24dc0404e35cd2
Vitaliy19891712 <stopkaiko@mail.ru>
2023-11-17 22:14:19 +0300

    change name folder and files

--- src/shared/ui/controlled/controlledCheckbox/ControlledCheckbox.tsx
+++ src/shared/ui/controlled/controlledCheckbox/ControlledCheckbox.tsx
@@ -1,7 +1,7 @@
 import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
 
 import { CommonCheckbox } from '../..'
-import { CheckboxPropsType } from '../../commonCheckbox/CommonCheckbox'
+import { CheckboxPropsType } from '../../СommonCheckbox/CommonCheckbox'
 
 type TypeControlledTextFieldProps<T extends FieldValues> = Omit<
   UseControllerProps<T>,
