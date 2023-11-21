4ad8d6985f593525b21c9bb4d6607a8f99968e02
Vitaliy19891712 <stopkaiko@mail.ru>
2023-11-21 16:02:15 +0300

    refactor: change commoncheckbox

--- src/shared/ui/controlled/controlledCheckbox/ControlledCheckbox.tsx
+++ src/shared/ui/controlled/controlledCheckbox/ControlledCheckbox.tsx
@@ -1,7 +1,6 @@
 import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
 
 import { CommonCheckbox } from '../..'
-
 import { CheckboxPropsType } from '@/shared/ui/CommonCheckbox/CommonCheckbox'
 
 type TypeControlledTextFieldProps<T extends FieldValues> = Omit<
