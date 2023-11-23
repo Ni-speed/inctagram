28ffd6ce970e4a429715fd789adb3ab3a54dce48
Ni-speed <railmir@gmail.com>
2023-11-23 19:56:24 +0500

    HOR-55 feat: replacing the div block with the HeroProfilePlaceholder component

--- src/widgets/profile/ui/Profile.tsx
+++ src/widgets/profile/ui/Profile.tsx
@@ -1,14 +1,15 @@
 import { FC } from 'react'
 
 import { Slider, Typography } from '@/shared'
+import { HeroProfilePlaceholder } from '@/widgets/heroProfilePlaceholder/HeroProfilePlaceholder'
 import { Sidebar } from '@/widgets/sidebar'
 import Image from 'next/image'
 
 import s from './profile.module.scss'
-type Props = {
+type PropsType = {
   user: { avatar: string; name: string }
 }
-export const Profile: FC<Props> = ({ user }) => {
+export const Profile: FC<PropsType> = ({ user }) => {
   return (
     <>
       <Image alt={'avatar'} className={s.avatar} height={204} src={user.avatar} width={204} />
@@ -37,88 +38,7 @@ export const Profile: FC<Props> = ({ user }) => {
           laboris nisi ut aliquip ex ea commodo consequat.
         </a>
       </div>
-      <div className={s.posts}>
-        <Slider
-          items={[
-            {
-              title: 'qwe',
-              url: 'https://cs11.livemaster.ru/storage/topic/NxN/48/72/51ca01280af65e4ef909ed524902f4cfaf44vc.jpg?h=YsmDTRGuAl-rn9pK1vTgCA',
-            },
-            {
-              title: 'qwe',
-              url: 'https://i.pinimg.com/736x/f4/d2/96/f4d2961b652880be432fb9580891ed62.jpg',
-            },
-            {
-              title: 'qwe',
-              url: 'https://img.championat.com/c/900x900/news/big/k/c/world-of-warcraft-wrath-of-the-lich-king-classic-reklamiruyut-milye-kotiki_16628211961568633755.jpg',
-            },
-          ]}
-        ></Slider>
-        <Slider
-          items={[
-            {
-              title: 'qwe',
-              url: 'https://cs11.livemaster.ru/storage/topic/NxN/48/72/51ca01280af65e4ef909ed524902f4cfaf44vc.jpg?h=YsmDTRGuAl-rn9pK1vTgCA',
-            },
-            {
-              title: 'qwe',
-              url: 'https://i.pinimg.com/736x/f4/d2/96/f4d2961b652880be432fb9580891ed62.jpg',
-            },
-            {
-              title: 'qwe',
-              url: 'https://img.championat.com/c/900x900/news/big/k/c/world-of-warcraft-wrath-of-the-lich-king-classic-reklamiruyut-milye-kotiki_16628211961568633755.jpg',
-            },
-          ]}
-        ></Slider>
-        <Slider
-          items={[
-            {
-              title: 'qwe',
-              url: 'https://cs11.livemaster.ru/storage/topic/NxN/48/72/51ca01280af65e4ef909ed524902f4cfaf44vc.jpg?h=YsmDTRGuAl-rn9pK1vTgCA',
-            },
-            {
-              title: 'qwe',
-              url: 'https://i.pinimg.com/736x/f4/d2/96/f4d2961b652880be432fb9580891ed62.jpg',
-            },
-            {
-              title: 'qwe',
-              url: 'https://img.championat.com/c/900x900/news/big/k/c/world-of-warcraft-wrath-of-the-lich-king-classic-reklamiruyut-milye-kotiki_16628211961568633755.jpg',
-            },
-          ]}
-        ></Slider>
-        <Slider
-          items={[
-            {
-              title: 'qwe',
-              url: 'https://cs11.livemaster.ru/storage/topic/NxN/48/72/51ca01280af65e4ef909ed524902f4cfaf44vc.jpg?h=YsmDTRGuAl-rn9pK1vTgCA',
-            },
-            {
-              title: 'qwe',
-              url: 'https://i.pinimg.com/736x/f4/d2/96/f4d2961b652880be432fb9580891ed62.jpg',
-            },
-            {
-              title: 'qwe',
-              url: 'https://img.championat.com/c/900x900/news/big/k/c/world-of-warcraft-wrath-of-the-lich-king-classic-reklamiruyut-milye-kotiki_16628211961568633755.jpg',
-            },
-          ]}
-        ></Slider>
-        <Slider
-          items={[
-            {
-              title: 'qwe',
-              url: 'https://cs11.livemaster.ru/storage/topic/NxN/48/72/51ca01280af65e4ef909ed524902f4cfaf44vc.jpg?h=YsmDTRGuAl-rn9pK1vTgCA',
-            },
-            {
-              title: 'qwe',
-              url: 'https://i.pinimg.com/736x/f4/d2/96/f4d2961b652880be432fb9580891ed62.jpg',
-            },
-            {
-              title: 'qwe',
-              url: 'https://img.championat.com/c/900x900/news/big/k/c/world-of-warcraft-wrath-of-the-lich-king-classic-reklamiruyut-milye-kotiki_16628211961568633755.jpg',
-            },
-          ]}
-        ></Slider>
-      </div>
+      <HeroProfilePlaceholder />
     </>
   )
 }
