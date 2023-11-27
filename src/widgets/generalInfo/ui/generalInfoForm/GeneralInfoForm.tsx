e97b833f044849839cb8d5fd85b582f9b2d2401d
IvanBichan <ivanbichanfe@gmail.com>
2023-11-27 23:47:12 +0100

    HOR-60-GeneralInfo feat: add request for Countries and cities

--- src/widgets/generalInfo/ui/generalInfoForm/GeneralInfoForm.tsx
+++ src/widgets/generalInfo/ui/generalInfoForm/GeneralInfoForm.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react'
+import React, { FormEvent, ReactElement, useEffect, useState } from 'react'
 import { SubmitHandler } from 'react-hook-form'
 
 import { ControlledInput, Select, useTranslation } from '@/shared'
@@ -9,6 +9,12 @@ import { useGeneralInfoForm } from '@/widgets/generalInfo/ui/generalInfoForm/use
 import { clsx } from 'clsx'
 
 import s from './GeneralInfoForm.module.scss'
+
+type CountryType = {
+  label: string
+  value: string
+}
+type CountriesList = CountryType[] | null
 
 type GeneralInfoFormProps = {
   className?: string
@@ -25,13 +31,95 @@ type GeneralInfoFormProps = {
 }
 export const GeneralInfoForm = ({ className, onSubmit, options }: GeneralInfoFormProps) => {
   //todo change the 'username' to the "username" that came from the backend
-  const { control, handleSubmit, register, setError } = useGeneralInfoForm(onSubmit, 'username')
+  const { control, handleSubmit, register, resetField, setError, watch } = useGeneralInfoForm(
+    onSubmit,
+    'username'
+  )
   const { t } = useTranslation()
-  const [birthday, setBirthday] = useState<Date | null>(null)
   const handleSubmitForm = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     await handleSubmit()
   }
+  const [birhD, setBirhD] = useState<any>()
+  const [countryList, setCountryList] = useState<CountriesList>(null)
+  const [citiesList, setCitiesList] = useState<any>(null)
+
+  useEffect(() => {
+    async function logMovies() {
+      try {
+        const response = await fetch('https://restcountries.com/v3.1/all')
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+
+        const data = await response.json()
+
+        const names = data.map((country: any) => {
+          return { label: country.name.common, value: country.name.common }
+        })
+
+        names.sort((a: CountryType, b: CountryType) => {
+          const nameA = a.label.toUpperCase()
+          const nameB = b.label.toUpperCase()
+
+          if (nameA < nameB) {
+            return -1
+          }
+          if (nameA > nameB) {
+            return 1
+          }
+
+          return 0
+        })
+
+        setCountryList(names)
+      } catch (error) {
+        console.error('Error fetching countries:')
+      }
+    }
+
+    logMovies()
+  }, [])
+
+  const contryFromForm = watch('country')?.toLocaleLowerCase()
+
+  useEffect(() => {
+    resetField('city')
+
+    async function fetchCities() {
+      const url = 'https://countriesnow.space/api/v0.1/countries/cities'
+      const requestBody = {
+        country: contryFromForm,
+      }
+
+      try {
+        const response = await fetch(url, {
+          body: JSON.stringify(requestBody),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+        })
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+
+        const citiesData = await response.json()
+
+        const citiesList = citiesData.data.map((city: string[]) => {
+          return { label: city, value: city }
+        })
+
+        setCitiesList(citiesList)
+      } catch (error) {
+        console.error('Error fetching cities:')
+      }
+    }
+
+    fetchCities()
+  }, [contryFromForm])
 
   return (
     <>
@@ -63,19 +151,27 @@ export const GeneralInfoForm = ({ className, onSubmit, options }: GeneralInfoFor
           label={t.generalInfo.lastname + '*'}
           name={'lastname'}
         />
-        {/* <DatePicker setStartDate={setBirthday} startDate={birthday} />*/}
-        <ControlledSelect
-          control={control}
-          label={t.generalInfo.country}
-          name={'country'}
-          options={options.data}
-        />
-        <ControlledSelect
-          control={control}
-          label={t.generalInfo.country}
-          name={'city'}
-          options={options.data}
-        />
+        <DatePicker className={s.dataPicker} setStartDate={setBirhD} startDate={birhD} />
+        <div className={s.area}>
+          <ControlledSelect
+            className={s.areaItem}
+            control={control}
+            label={t.generalInfo.country}
+            name={'country'}
+            options={countryList}
+            placeholder={'Country'}
+            rootClassName={s.areaItem}
+          />
+          <ControlledSelect
+            className={s.areaItem}
+            control={control}
+            label={t.generalInfo.country}
+            name={'city'}
+            options={citiesList}
+            placeholder={'City'}
+            rootClassName={s.areaItem}
+          />
+        </div>
         <ControlledTextArea control={control} label={t.generalInfo.aboutMe} name={'aboutMe'} />
 
         <button type={'submit'}>submit</button>
