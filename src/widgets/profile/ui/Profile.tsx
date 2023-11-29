ad580445b01bfd090df9253712d74d9e4a50d1dc
Ni-speed <railmir@gmail.com>
2023-11-29 20:24:45 +0500

    HOR-55 style: add style for Profile component

--- src/widgets/profile/ui/Profile.tsx
+++ src/widgets/profile/ui/Profile.tsx
@@ -1,42 +1,47 @@
 import { FC } from 'react'
 
-import { Slider, Typography } from '@/shared'
+import { Typography } from '@/shared'
 import { HeroProfilePlaceholder } from '@/widgets/heroProfilePlaceholder/HeroProfilePlaceholder'
-import { Sidebar } from '@/widgets/sidebar'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import s from './profile.module.scss'
+
 type PropsType = {
   user: { avatar: string; name: string }
 }
 export const Profile: FC<PropsType> = ({ user }) => {
   return (
     <>
-      <Image alt={'avatar'} className={s.avatar} height={204} src={user.avatar} width={204} />
-      <Typography>URLProfile</Typography>
-      <ul>
-        <li>
-          <span className={s.counterStats}>2218</span>
-          <Typography className={s.listText}>Following</Typography>
-        </li>
-        <li>
-          <span className={s.counterStats}>3358</span>
-          <Typography className={s.listText}>Followers</Typography>
-        </li>
-        <li>
-          <span className={s.counterStats}>4764</span>
-          <Typography className={s.listText}>Publications</Typography>
-        </li>
-      </ul>
-      <div className={s.textBlock}>
-        <span className={s.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco
-        </span>
-        <a className={s.link} href={'#'}>
-          laboris nisi ut aliquip ex ea commodo consequat.
-        </a>
+      <div className={s.profilePageHeaderWrapper}>
+        <Image alt={'avatar'} className={s.avatar} height={204} src={user.avatar} width={204} />
+        <div className={s.statisticWrapper}>
+          <Typography>URLProfile</Typography>
+          <ul className={s.profileStatistics}>
+            <li className={s.statistic}>
+              <span className={s.counterStats}>2218</span>
+              <Typography className={s.listText}>Following</Typography>
+            </li>
+            <li className={s.statistic}>
+              <span className={s.counterStats}>3358</span>
+              <Typography className={s.listText}>Followers</Typography>
+            </li>
+            <li className={s.statistic}>
+              <span className={s.counterStats}>4764</span>
+              <Typography className={s.listText}>Publications</Typography>
+            </li>
+          </ul>
+          <div className={s.textBlock}>
+            <span className={s.text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco
+            </span>
+            <Link className={s.link} href={'#'}>
+              laboris nisi ut aliquip ex ea commodo consequat.
+            </Link>
+          </div>
+        </div>
       </div>
       <HeroProfilePlaceholder />
     </>
