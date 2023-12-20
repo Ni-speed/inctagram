e63b446daac22dc616dcc66ae939acd44c41931f
IvanBichan <ivanbichanfe@gmail.com>
2023-12-20 13:39:04 +0100

    HOR-60-GeneralInfo feat: add ProfileImageWithCrop

--- src/widgets/generalInfo/ui/generalInfoForm/GeneralInfoForm.tsx
+++ src/widgets/generalInfo/ui/generalInfoForm/GeneralInfoForm.tsx
@@ -6,7 +6,9 @@ import { Button, ControlledDataPicker, ControlledInput, Typography, useTranslati
 import { ControlledSelect } from '@/shared/ui/controlled/controlledSelect/ControlledSelect'
 import { ControlledTextArea } from '@/shared/ui/controlled/controlledTextArea/ControlledTextArea'
 import { useGeneralInfoForm } from '@/widgets/generalInfo/ui/generalInfoForm/useGeneralInfoForm'
-import { ProfileImage } from '@/widgets/generalInfo/ui/profileImage/ProfileImage'
+import { getCities } from '@/widgets/generalInfo/ui/getCities'
+import { CountriesList, getCountries } from '@/widgets/generalInfo/ui/getContries'
+import { ProfileImageWithCrop } from '@/widgets/generalInfo/ui/profileImage/ProfileImageWithCrop'
 import { clsx } from 'clsx'
 
 import s from './GeneralInfoForm.module.scss'
@@ -14,29 +16,23 @@ import s from './GeneralInfoForm.module.scss'
 export type ProfileProps = {
   aboutMe?: null | string
   avatars?: AvatarType[]
+  birthDate?: Date
   city?: null | string
   createdAt?: Date
-  dateOfBirth?: Date
   firstname: string
   id?: number
   lastname: string
   username: null | string
 }
 
-type CountryType = {
-  label: string
-  value: string
-}
-type CountriesList = CountryType[] | null
-
 type GeneralInfoFormProps = {
   className?: string
   myUserName?: string
   onSubmitProfile: SubmitHandler<{
     aboutMe: null | string
+    birthDate: Date | string
     city: null | string
     country: null | string
-    dateOfBirth: Date | string
     firstname: string
     lastname: string
     username: null | string
@@ -52,7 +48,7 @@ export const GeneralInfoForm = ({
 }: GeneralInfoFormProps) => {
   //todo change the 'username' to the "username" that came from the backend
   const { t } = useTranslation()
-  const { control, handleSubmit, register, resetField, setError, watch } = useGeneralInfoForm(
+  const { control, handleSubmit, resetField, setError, trigger, watch } = useGeneralInfoForm(
     onSubmitProfile,
     profile,
     myUserName
@@ -67,90 +63,24 @@ export const GeneralInfoForm = ({
   const [citiesList, setCitiesList] = useState<any>(null)
 
   useEffect(() => {
-    async function logMovies() {
-      try {
-        const response = await fetch('https://restcountries.com/v3.1/all')
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`)
-        }
-
-        const data = await response.json()
-
-        const names = data.map((country: any) => {
-          return { label: country.name.common, value: country.name.common }
-        })
-
-        names.sort((a: CountryType, b: CountryType) => {
-          const nameA = a.label.toUpperCase()
-          const nameB = b.label.toUpperCase()
-
-          if (nameA < nameB) {
-            return -1
-          }
-          if (nameA > nameB) {
-            return 1
-          }
-
-          return 0
-        })
-
-        setCountryList(names)
-      } catch (error) {
-        console.error('Error fetching countries:')
-      }
-    }
-
-    logMovies()
+    getCountries(setCountryList)
   }, [])
 
-  const contryFromForm = watch('country')?.toLocaleLowerCase()
+  const countryFromForm = watch('country')?.toLocaleLowerCase()
 
   useEffect(() => {
-    resetField('city')
-
-    async function fetchCities() {
-      const url = 'https://countriesnow.space/api/v0.1/countries/cities'
-      const requestBody = {
-        country: contryFromForm,
-      }
-
-      try {
-        const response = await fetch(url, {
-          body: JSON.stringify(requestBody),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          method: 'POST',
-        })
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`)
-        }
-
-        const citiesData = await response.json()
-
-        const citiesList = citiesData.data.map((city: string[]) => {
-          return { label: city, value: city }
-        })
-
-        setCitiesList(citiesList)
-      } catch (error) {
-        console.error('Error fetching cities:')
-      }
+    if (countryFromForm) {
+      resetField('city', { defaultValue: '' })
+      getCities(countryFromForm, setCitiesList)
     }
-
-    if (contryFromForm) {
-      fetchCities()
-    }
-  }, [contryFromForm])
+  }, [countryFromForm, resetField])
 
   return (
     <>
       <form className={clsx(s.form, className)} onSubmit={handleSubmitForm}>
         <div className={s.container}>
           <div className={s.profileImage}>
-            <ProfileImage avatars={profile?.avatars && profile?.avatars[0]?.url} />
+            <ProfileImageWithCrop avatars={profile?.avatars && profile?.avatars[0]?.url} />
           </div>
           <div className={s.profileForm}>
             <ControlledInput
@@ -184,7 +114,7 @@ export const GeneralInfoForm = ({
               className={s.dataPicker}
               control={control}
               label={t.generalInfo.dateOfBirth}
-              name={'dateOfBirth'}
+              name={'birthDate'}
             />
             <div className={s.area}>
               <ControlledSelect
