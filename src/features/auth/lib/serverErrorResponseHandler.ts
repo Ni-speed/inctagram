3602f4102d1d666f5ae4fdf9143709becfd01977
IvanBichan <ivanbichanfe@gmail.com>
2024-03-02 01:19:26 +0100

    HOR-60-GeneralInfo fix: change data for error

--- src/features/auth/lib/serverErrorResponseHandler.ts
+++ src/features/auth/lib/serverErrorResponseHandler.ts
@@ -3,10 +3,11 @@ import { UseFormSetError } from 'react-hook-form'
 import { FormSignUp } from '../ui/signUp/lib/useRegisterForm'
 import { FormSignIn } from '../ui/singIn/lib/useSingInForm'
 import { LocaleType } from '@/../locales/ru'
-import { ErrorResponse } from '@/features/auth/model/types'
+import { ErrorResponse, NewErrorSignUpResponse } from '@/features/auth/model/types'
 
 type ServerErrorSignUpHandler = {
-  code: number
+  code?: number
+  errorResponse: NewErrorSignUpResponse
   serverErrorHandler: (t: LocaleType) => ErrorResponse
   setError: UseFormSetError<FormSignIn | FormSignUp>
   t: LocaleType
@@ -14,12 +15,21 @@ type ServerErrorSignUpHandler = {
 
 export const serverErrorResponseHandler = ({
   code,
+  errorResponse,
   serverErrorHandler,
   setError,
   t,
 }: ServerErrorSignUpHandler) => {
   const errors = serverErrorHandler(t)
 
+  //todo fix problem with types
+  if (errorResponse.data.messages) {
+    typeof errorResponse.data.messages === 'string'
+      ? setError('email', { message: errorResponse.data.messages, type: 'email' })
+      : errorResponse.data.messages?.map(error => {
+          setError(error.field, { message: error.message, type: error.field })
+        })
+  }
   errors.map(e => {
     e.type === String(code) &&
       setError(e.name, {
